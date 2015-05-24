f3b55dbab12dc6f21497527180ac67aa0b881878
Guney Can Gokoglu <guneycan@gmail.com>
2015-05-24 12:47:56 +0300

    added new migrations for users, cities, districts and hoods.

---
+++ database/migrations/2015_05_24_124404_create_hoods_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateHoodsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('hoods', function (Blueprint $table) {
+			$table->increments('id');
+			$table->integer('city_id')->unsigned();
+			$table->integer('district_id')->unsigned();
+			$table->string('name');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::drop('hoods');
+	}
+
+}
