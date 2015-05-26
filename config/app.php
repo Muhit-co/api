806bc492df1a93d04190c3dee6a1947970feef31
Guney Can Gokoglu <guneycan@gmail.com>
2015-05-26 19:16:09 +0300

    saved app.php

--- config/app.php
+++ config/app.php
@@ -151,6 +151,7 @@ return [
 
 		'LucaDegasperi\OAuth2Server\Storage\FluentStorageServiceProvider',
 		'LucaDegasperi\OAuth2Server\OAuth2ServerServiceProvider',
+		'Barryvdh\Cors\CorsServiceProvider',
 
 	],
 
