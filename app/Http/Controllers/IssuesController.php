0a76875fc0ffd4f85d9cc30e22ac5a9cdb99c709
Guney Can Gokoglu <guneycan@gmail.com>
2015-05-26 20:06:29 +0300

    creating a new issue

--- app/Http/Controllers/IssuesController.php
+++ app/Http/Controllers/IssuesController.php
@@ -1,7 +1,13 @@
 <?php namespace Muhit\Http\Controllers;
 
+use Authorizer;
+use Carbon\Carbon;
+use DB;
 use Muhit\Http\Controllers\Controller;
+use Muhit\Models\Issue;
+use Redis;
 use Request;
+use Storage;
 
 class IssuesController extends Controller {
 
@@ -13,6 +19,90 @@ class IssuesController extends Controller {
 	 **/
 	public function postAdd() {
 		$data = Request::all();
+		$user_id = Authorizer::getResourceOwnerId();
+
+		$required_fields = ['tags', 'title', 'desc', 'location'];
+
+		foreach ($required_fields as $key) {
+			if (!isset($data[$key]) or empty($data[$key])) {
+				return response()->api(400, 'Missing fields, ' . $key . ' is required', $data);
+			}
+		}
+
+		DB::beginTransaction();
+		#save the issue;
+
+		$issue = new Issue;
+		$issue->user_id = $user_id;
+		$issue->title = $data['title'];
+		$issue->desc = $data['desc'];
+		$issue->status = 'new';
+		$issue->location = $data['location'];
+		$issue->city_id = 0;
+		$issue->district_id = 0;
+		$issue->hood_id = 0;
+
+		try {
+			$issue->save();
+		} catch (Exception $e) {
+			Log::error('IssuesController/postAdd/SavingIssue', (array) $e);
+			DB::rollback();
+			return response()->api(500, 'Error on saving the issue', $data);
+		}
+
+		#save the tags
+		if (!empty($data['tags']) and is_array($data['tags'])) {
+			foreach ($data['tags'] as $tag) {
+				try {
+					DB::table('issue_tag')->insert([
+						'issue_id' => $issue->id,
+						'tag_id' => $tag,
+						'created_at' => Carbon::now(),
+						'updated_at' => Carbon::now(),
+					]);
+					Redis::incr('tag_issue_counter:' . $tag);
+				} catch (Exception $e) {
+					Log::error('IssuesController/postAdd/SavingTagRelation', (array) $e);
+					DB::rollback();
+					return response()->api(500, 'Error on saving the issue', $data);
+				}
+			}
+		}
+
+		#save the images
+		if (!empty($data['images']) and is_array($data['images'])) {
+			foreach ($data['images'] as $image) {
+				try {
+					$name = str_replace('.', '', microtime(true));
+					Storage::put('issues/' . $name, base64_decode($image));
+					DB::table('issue_images')->insert([
+						'issue_id' => $issue->id,
+						'image' => 'issues/' . $name,
+						'created_at' => Carbon::now(),
+						'updated_at' => Carbon::now(),
+					]);
+				} catch (Exception $e) {
+					Log::error('IssuesController/postAdd/SavingTheImage', (array) $e);
+				}
+			}
+		}
+
+		try {
+			DB::table('issue_updates')->insert([
+				'user_id' => $user_id,
+				'issue_id' => $issue->id,
+				'old_status' => 'n/a',
+				'new_status' => 'new',
+				'created_at' => Carbon::now(),
+				'updated_at' => Carbon::now(),
+			]);
+		} catch (Exception $e) {
+			Log::error('IssuesController/postAdd/SavingIssueUpdate', (array) $e);
+		}
+
+		DB::commit();
+
+		return response()->api(200, 'Issue saved', Issue::with('user', 'tags', 'images')->find($issue->id));
 
 	}
 
--- app/Models/Issue.php
+++ app/Models/Issue.php
@@ -6,4 +6,18 @@ class Issue extends Model {
 
 	//
 
+	protected $guarded = ['id'];
+
+	public function tags() {
+		return $this->belongsToMany('Muhit\Models\Tag');
+	}
+
+	public function user() {
+		return $this->belongsTo('Muhit\Models\User');
+	}
+
+	public function images() {
+		return $this->hasMany('Muhit\Models\IssueImage');
+	}
+
 }
