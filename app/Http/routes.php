a36998a607a29c44d136ec2fbb6caf8cb33fb533
Guney Can Gokoglu <guneycan@gmail.com>
2015-06-14 19:13:36 +0300

    added api routeing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,39 +11,46 @@
 |
  */
 
-Route::get('/', function () {
-	return response()->api(200, "Welcome to the Muhit API. ");
+Route::get('/', function() {
+    return view('pages.home');
 });
 
-Route::get('/secure', ['before' => 'oauth', function () {
-	return response()->api(500, "You have access to the secure calls");
-}]);
+Route::group(['prefix' => 'api'], function () {
+    Route::get('/', function () {
+        return response()->api(200, "Welcome to the Muhit API. ");
+    });
 
-Route::controller('/auth', 'AuthController');
-Route::controller('/hoods', 'HoodsController');
+    Route::get('/secure', ['before' => 'oauth', function () {
+        return response()->api(500, "You have access to the secure calls");
+    }]);
 
-Route::get('tags/{q?}', 'TagsController@getList');
-Route::get('announcements/{hood_id}/{start?}/{take?}', 'AnnouncementsController@getList');
+    Route::controller('/auth', 'AuthController');
+    Route::controller('/hoods', 'HoodsController');
 
-Route::get('/issues/view/{id}', 'IssuesController@getView');
-Route::get('/issues/list/{start?}/{take?}', 'IssuesController@getList');
-Route::post('/issues/search', 'IssuesController@postSearch');
-Route::get('/issues/popular/{start?}/{take?}', 'IssuesController@getPopular');
-Route::get('/issues/latest/{start?}/{take?}', 'IssuesController@getLatest');
-Route::get('/issues/by-tag/{tag_id}/{start?}/{take?}', 'IssuesController@getByTag');
-Route::get('/issues/by-hood/{hood_id}/{start?}/{take?}', 'IssuesController@getByHood');
-Route::get('/issues/by-district/{district_id}/{start?}/{take?}', 'IssuesController@getByDistrict');
-Route::get('/issues/by-city/{city_id}/{start?}/{take?}', 'IssuesController@getByCity');
-Route::get('/issues/by-user/{user_id}/{start?}/{take?}', 'IssuesController@getByUser');
-Route::get('/issues/by-status/{status}/{start?}/{take?}', 'IssuesController@getByStatus');
-Route::get('/issues/by-supporter/{user_id}/{start?}/{take?}', 'IssuesController@getBySupporter');
+    Route::get('tags/{q?}', 'TagsController@getList');
+    Route::get('announcements/{hood_id}/{start?}/{take?}', 'AnnouncementsController@getList');
 
-Route::get('/profile/{user_id?}', 'MembersController@getProfile');
+    Route::get('/issues/view/{id}', 'IssuesController@getView');
+    Route::get('/issues/list/{start?}/{take?}', 'IssuesController@getList');
+    Route::post('/issues/search', 'IssuesController@postSearch');
+    Route::get('/issues/popular/{start?}/{take?}', 'IssuesController@getPopular');
+    Route::get('/issues/latest/{start?}/{take?}', 'IssuesController@getLatest');
+    Route::get('/issues/by-tag/{tag_id}/{start?}/{take?}', 'IssuesController@getByTag');
+    Route::get('/issues/by-hood/{hood_id}/{start?}/{take?}', 'IssuesController@getByHood');
+    Route::get('/issues/by-district/{district_id}/{start?}/{take?}', 'IssuesController@getByDistrict');
+    Route::get('/issues/by-city/{city_id}/{start?}/{take?}', 'IssuesController@getByCity');
+    Route::get('/issues/by-user/{user_id}/{start?}/{take?}', 'IssuesController@getByUser');
+    Route::get('/issues/by-status/{status}/{start?}/{take?}', 'IssuesController@getByStatus');
+    Route::get('/issues/by-supporter/{user_id}/{start?}/{take?}', 'IssuesController@getBySupporter');
 
-Route::group(['before' => ['oauth']], function () {
-	Route::post('/issues/add', 'IssuesController@postAdd');
-	Route::post('/issues/support/{id}', 'IssuesController@getSupport');
-	Route::post('/issues/unsupport/{id}', 'IssuesController@getUnSupport');
-    Route::post('/issues/comment/', 'IssuesController@postComment');
-    Route::controller('/members', 'MembersController');
+    Route::get('/profile/{user_id?}', 'MembersController@getProfile');
+
+    Route::group(['before' => ['oauth']], function () {
+        Route::post('/issues/add', 'IssuesController@postAdd');
+        Route::post('/issues/support/{id}', 'IssuesController@getSupport');
+        Route::post('/issues/unsupport/{id}', 'IssuesController@getUnSupport');
+        Route::post('/issues/comment/', 'IssuesController@postComment');
+        Route::controller('/members', 'MembersController');
+    });
+
 });
