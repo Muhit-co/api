66adfc0b8e5a6c7b237b4bc5b9f095ae2785bfae
Daniel Swakman <dswakman@oefenweb.nl>
2015-07-05 23:15:21 +0300

    added iOS homescreen assets

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,31 +1,35 @@
 <head>
 
-  <title>{{ $pageTitle or '' }} MUHIT</title>
+    <title>{{ $pageTitle or '' }} MUHIT</title>
 
-  <meta name="description" content="" />
-  <meta name="keywords" content="" />
-  <meta name="author" content="L Daniel Swakman, www.ldaniel.eu" />
-  <meta charset="utf-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="L Daniel Swakman, www.ldaniel.eu" />
+    <meta charset="utf-8" />
 
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
-  <?php $env = env('APP_ENV') ?>
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,400italic" />
+    <?php $env = env('APP_ENV') ?>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,400italic" />
 
-  <link rel="stylesheet" href="/css/app.css" />
-  <script src="/js/all.js"></script>
+    <link rel="stylesheet" href="/css/app.css" />
+    <script src="/js/all.js"></script>
 
-  <link href="/images/favicon.ico" type="image/x-icon" rel="icon" />
-  <link href="/images/favicon.ico" type="image/x-icon" rel="shortcut icon" />
+    <link href="/images/favicon.ico" type="image/x-icon" rel="icon" />
+    <link href="/images/favicon.ico" type="image/x-icon" rel="shortcut icon" />
 
-  <link rel="manifest" href="/manifest.json">
-  <meta name="mobile-web-app-capable" content="yes">
-  <meta name="theme-color" content="#44a1e0">
-  <meta name="msapplication-navbutton-color" content="#44a1e0">
-  <meta name="apple-mobile-web-app-status-bar-style" content="#44a1e0">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="theme-color" content="#44a1e0">
+    <meta name="msapplication-navbutton-color" content="#44a1e0">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#44a1e0">
+    <link rel="apple-touch-icon" href="/images/app-icons/launcher-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/images/app-icons/launcher-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/images/app-icons/launcher-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/images/app-icons/launcher-icon-152x152.png">
 
-    <script type="text/javascript"
-      src="//maps.googleapis.com/maps/api/js?libraries=places">
+    <script type="text/javascript" src="//maps.googleapis.com/maps/api/js?libraries=places">
     </script>
 
 </head>
