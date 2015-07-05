27a3bccd4ca39b50b34e40f8a08f9b23f166a565
Guney Can Gokoglu <guneycan@gmail.com>
2015-07-05 16:53:29 +0300

    added counters to user and issues

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,30 +3,38 @@
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
+use Redis;
 
 class User extends Model implements AuthenticatableContract {
 
-	use Authenticatable;
+    use Authenticatable;
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
 
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array
-	 */
-	protected $fillable = ['first_name', 'email', 'password', 'last_name', 'picture', 'username'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['first_name', 'email', 'password', 'last_name', 'picture', 'username'];
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = ['password', 'remember_token', 'level'];
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['password', 'remember_token', 'level'];
+
+    public function toArray() {
+        $array = parent::toArray();
+        $array['opened_issue_counter'] = (int) Redis::get('user_opened_issue_counter:' . $this->id);
+        $array['supported_issue_counter'] = (int) Redis::get('user_opened_issue_counter:' . $this->id);
+        return $array;
+    }
 
 }
