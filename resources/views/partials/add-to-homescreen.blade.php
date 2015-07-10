003e00e11cced1528822f79aaeb553f63647d128
Daniel Swakman <dswakman@oefenweb.nl>
2015-07-10 12:58:08 +0300

    added google analytics tracker

---
+++ resources/views/partials/add-to-homescreen.blade.php
@@ -0,0 +1,10 @@
+<link rel="manifest" href="/manifest.json">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="theme-color" content="#44a1e0">
+<meta name="msapplication-navbutton-color" content="#44a1e0">
+<meta name="apple-mobile-web-app-status-bar-style" content="#44a1e0">
+<link rel="apple-touch-icon" href="/images/app-icons/launcher-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/images/app-icons/launcher-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/images/app-icons/launcher-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/images/app-icons/launcher-icon-152x152.png">
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -18,16 +18,8 @@
     <link href="/images/favicon.ico" type="image/x-icon" rel="icon" />
     <link href="/images/favicon.ico" type="image/x-icon" rel="shortcut icon" />
 
-    <link rel="manifest" href="/manifest.json">
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="theme-color" content="#44a1e0">
-    <meta name="msapplication-navbutton-color" content="#44a1e0">
-    <meta name="apple-mobile-web-app-status-bar-style" content="#44a1e0">
-    <link rel="apple-touch-icon" href="/images/app-icons/launcher-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/images/app-icons/launcher-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/images/app-icons/launcher-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/images/app-icons/launcher-icon-152x152.png">
+    @include('partials.add-to-homescreen')
+    @include('partials.google-analytics')
 
     <script type="text/javascript" src="//maps.googleapis.com/maps/api/js?libraries=places">
     </script>
