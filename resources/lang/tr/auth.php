74efb9923164c32daaa8f9ee0620751a88b05238
Daniel Swakman <dswakman@oefenweb.nl>
2015-07-19 18:05:45 +0300

    added translation strings

--- resources/lang/tr/auth.php
+++ resources/lang/tr/auth.php
@@ -4,6 +4,8 @@ return [
     "log_in" => "Giriş yap",
     "log_in_cap" => "GİRİŞ YAP",
     "log_in_short_cap" => "GİRİŞ",
+    "sign_up" => "Kayıt ol",
+    "sign_up_cap" => "KAYIT OL",
     "connect_cap" => "BAĞLAN",
     "muhtar_signup_title" => "Mahalleni aktif hale getir",
     "neighbourhood" => "Mahalleniz",
@@ -17,5 +19,6 @@ return [
     "password" => "Şifreniz",
     "password_placeholder" => "En az 8 karakter",
     "agree_with_terms" => ":tagstartKullanım koşulları:tagend kabul ediyorum.",
-    "request_access_cap" => "GİRİŞ İZNİ İSTE"
+    "request_access_cap" => "GİRİŞ İZNİ İSTE",
+    "cancel_cap" => "VAZGEÇ",
 ];
