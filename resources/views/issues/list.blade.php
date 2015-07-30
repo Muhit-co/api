b2f77257a56ca434c6b2464dc68c9cc21b1978b7
Guney Can Gokoglu <guneycan@gmail.com>
2015-07-30 15:59:22 +0300

    map and list

--- resources/views/issues/list.blade.php
+++ resources/views/issues/list.blade.php
@@ -71,24 +71,6 @@
     </div>
 </section>
 
-<section id="map" class="tabsection u-opacity0">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-
-            <div id="map-canvas">
-                <!-- <iframe width="600" height="450" frameborder="0" style="border:0" src=""></iframe> -->
-                <script>
-                    // google.maps.event.addDomListener(window, 'load', mapInitialize);
-                </script>
-            </div>
-
-            <div class="u-aligncenter u-pv20">
-                <a class="btn btn-secondary" id="map_redraw">MAP REDRAW</a>
-            </div>
-
-        </div>
-    </div>
-</section>
 
 @include('partials.web-app-message')
 
--- resources/views/issues/map.blade.php
+++ resources/views/issues/map.blade.php
@@ -12,6 +12,7 @@
             <div id="map-canvas">
                 <!-- <iframe width="600" height="450" frameborder="0" style="border:0" src=""></iframe> -->
                 <script>
+                    mapInitialize();
                     // google.maps.event.addDomListener(window, 'load', mapInitialize);
                 </script>
             </div>
