bfa928cc3455f9275cc966bb0ffc12a80e9cc450
Guney Can Gokoglu <guneycan@gmail.com>
2015-09-06 18:26:30 +0300

    naked domain redirects

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	 */
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => 'info@muhit.co', 'name' => 'Muhit.co'],
 
 	/*
 	|--------------------------------------------------------------------------
