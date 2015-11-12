99e946a5f32e661a6c33f84c295c62fd2879b17b
Daniel Swakman <dswakman@oefenweb.nl>
2015-11-12 17:53:41 +0200

    start of map fitbounds implementation

--- resources/assets/js/maps.js
+++ resources/assets/js/maps.js
@@ -25,9 +25,31 @@ function mapInitialize() {
             icon: { url: '/images/map-icons/marker_' + status + '.png', size: new google.maps.Size(29, 41) }
         });
     });
-    map.data.addListener('click', function(event) {
-        window.location.href = '/issues/view/' + event.feature.getProperty('id');
-    });
+
+    // // Declare your bounds
+    // var bounds = new google.maps.LatLngBounds();
+
+    // $.getJSON('/mapdata.json', {}, function (data) {
+    //     $.each(data.features, function (i, marker) {
+    //         // Get coordinates from json object
+    //         var item = marker.geometry.coordinates;
+    //         console.log(item);
+    //         // Declare lat/long 
+    //         var latlng = new google.maps.LatLng(item[0], item[1]);
+    //         // Add lat/long to bounds
+    //         bounds.extend(latlng);
+    //     });
+    // });
+
+    // console.log(bounds);
+
+    // // Fit map to bounds.
+    //  map.fitBounds(bounds);
+
+
+    // map.data.addListener('click', function(event) {
+    //     window.location.href = '/issues/view/' + event.feature.getProperty('id');
+    // });
 }
 
 function mapInitializeForIssue(lon, lan) {
