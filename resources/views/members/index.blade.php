4a054c8235d01cafbc8fc6970cebc5f940491ef4
Daniel Swakman <dswakman@oefenweb.nl>
2015-12-02 10:28:31 +0200

    tabs for user management page

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -4,141 +4,179 @@
 <?php 
 // Clears $url from given query parameter $key
 function clearParam($url, $key) { 
-	$url = preg_replace('/(.*)(?|&)' . $key . '=[^&]+?(&)(.*)/i', '$1$2$4', $url . '&'); 
-	$url = substr($url, 0, -1); 
-	return $url; 
+    $url = preg_replace('/(.*)(?|&)' . $key . '=[^&]+?(&)(.*)/i', '$1$2$4', $url . '&'); 
+    $url = substr($url, 0, -1); 
+    return $url; 
 }
 
+// array with currently active levels
 $levels = array(
-	array(
-		'id' => '', 
-		'name' => '[none]'
-	),
-	array(
-		'id' => 0, 
-		'name' => 'User'
-	),
-	array(
-		'id' => 3, 
-		'name' => 'Rejected muhtar'
-	),
-	array(
-		'id' => 4, 
-		'name' => 'Pending muhtar'
-	),
-	array(
-		'id' => 5, 
-		'name' => 'Muhtar'
-	),
-	array(
-		'id' => 10, 
-		'name' => 'Admin'
-	),
+    array(
+        'id' => null,
+        'name' => 'All'
+    ),
+    array(
+        'id' => 0, 
+        'name' => 'Users'
+    ),
+    array(
+        'id' => 3, 
+        'name' => 'Rejected muhtars'
+    ),
+    array(
+        'id' => 4, 
+        'name' => 'Pending muhtars'
+    ),
+    array(
+        'id' => 5, 
+        'name' => 'Muhtars'
+    ),
+    array(
+        'id' => 10, 
+        'name' => 'Admins'
+    ),
 );
 ?>
 
+<header class="u-relative header-show">
+    <div class="row">
+        <div class="col-xs-12">
+
+            <div class="u-clearfix">
+
+                <div class="u-floatright">
+                    <?php 
+                    $param_q = (isset($_GET["q"])) ? $_GET["q"] : '';
+                    $param_loc = (isset($_GET["location"])) ? $_GET["location"] : '';
+                    ?>
+                    <form method="get">
+                        
+                        <div class="form-group u-floatleft u-width150">
+                            <input class="form-input" name="q" type="text" placeholder="Search..." value="<?php echo $param_q ?>" />
+                            @if (strlen($param_q) > 0)
+                                <a href="<?php echo '?' . buildRelativeUrl('q', '') ?>" class="form-appendRight u-ph15" style="padding-top: 8px;"><i class="ion ion-close"></i></a>
+                            @endif
+                        </div>
+
+                        <!-- <div class="form-group u-floatleft u-mr20 u-width150">
+                            <input name="location" type="text" placeholder="Location..." value="<?php echo $param_loc ?>" />
+                        </div>
+
+                        <div class="form-group u-floatleft u-mt5 u-width5p">
+                            <select name="level">
+                                @foreach($levels as $l)
+                                <option value="{{ $l['id'] }}">{{ $l['name'] }}</option>
+                                </option>
+                                @endforeach
+                            </select>
+                        </div> -->
+
+                        <input type="submit" style="position: absolute; left: -9999px"/>
+
+                    </form>
+                </div>
+
+                <h2>Users</h2>
+
+            </div>
+
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12">
+            @if(count($levels) > 0)
+                <?php 
+                $active_tab = (isset($_GET['level'])) ? intval($_GET['level']) : null;
+                ?>
+                <ul class="tabs">
+                    @foreach($levels as $l)
+                    <li>
+                        <a href="<?php echo '?' . buildRelativeUrl('level', $l['id']) ?>" <?php echo ($active_tab === $l['id']) ? 'class="active"' : ''; ?> >
+                            {{ strtoupper($l['name']) }}
+                        </a>
+                    </li>
+                    @endforeach
+                </ul>
+            @endif
+        </div>
+    </div>
+
+</header>
+
 <section id="members">
-	<div class="row">
-		<div class="col-xs-12 u-pv20">
+    <div class="row">
+        <div class="col-xs-12 u-pv20">
 
-			<div class="u-clearfix">
+            @if (strlen($param_q) > 0)
+                <em class="u-block u-mb10">
+                    <small>There are {{ count($members) }} results matching your criteria</small>
+                </em>
+            @endif
 
-				<div class="u-floatright">
-					<?php 
-					$param_q = (isset($_GET["q"])) ? $_GET["q"] : '';
-					$param_loc = (isset($_GET["location"])) ? $_GET["location"] : '';
-					?>
-					<form method="get">
-
-						<!-- <a href="<?php clearParam($_SERVER['REQUEST_URI'], $param_q) ?>" class="btn btn-sm u-floatright"><?php echo $_SERVER['REQUEST_URI'] ?></a> -->
-
-		                <div class="form-group u-floatleft u-mr20 u-width150">
-		                    <input name="q" type="text" placeholder="Search..." value="<?php echo $param_q ?>" />
-		                </div>
-		                <div class="form-group u-floatleft u-mr20 u-width150">
-							<input name="location" type="text" placeholder="Location..." value="<?php echo $param_loc ?>" />
-						</div>
-
-						<div class="form-group u-floatleft u-mt5 u-width5p">
-							<select name="level">
-								@foreach($levels as $l)
-								<option value="{{ $l['id'] }}">{{ $l['name'] }}</option>
-								</option>
-								@endforeach
-							</select>
-						</div>
-
-						<input type="submit" style="position: absolute; left: -9999px"/>
-
-					</form>
-				</div>
-
-				<h2>Users</h2>
-
-			</div>
-
-			<div class="list list-small u-mv20">
+            <div class="list list-small">
                 <div class="list-header">
                     <div class="u-floatleft u-width40">
-                    	<p>&nbsp;</p>
+                        <p>&nbsp;</p>
                     </div>
                     <div class="u-floatright" style="width: 107px;">
-                    	<small class="c-medium"><em>actions</em></small>
+                        <small class="c-medium"><em>actions</em></small>
                     </div>
                     <div class="u-nowrap">
-						<small class="u-floatleft u-width20p u-nowrap c-medium"><em>name</em></small>
-						<small class="u-floatleft u-width10p u-nowrap c-medium"><em>username</em></small>
-						<small class="u-floatleft u-width20p u-nowrap c-medium"><em>email</em></small>
-						<small class="u-floatleft u-width10p u-nowrap c-medium"><em>level</em></small>
-						<small class="u-floatleft u-width15p u-nowrap c-medium"><em>creation date</em></small>
-						<small class="u-floatleft u-width25p u-nowrap c-medium"><em>location</em></small>
+                        <small class="u-floatleft u-width30p u-nowrap c-medium"><em>name (username)</em></small>
+                        <small class="u-floatleft u-width20p u-nowrap c-medium"><em>email</em></small>
+                        <small class="u-floatleft u-width10p u-nowrap c-medium"><em>level</em></small>
+                        <small class="u-floatleft u-width15p u-nowrap c-medium"><em>creation date</em></small>
+                        <small class="u-floatleft u-width25p u-nowrap c-medium"><em>location</em></small>
                     </div>
                 </div>
                 <ul class="list-content">
 
                     @foreach($members as $m)
-                    <li>
-                        <a href="/admin/view-member/{{$m->id}}">
-							<div class="badge badge-circle-small u-floatleft u-mt5 u-ml5 u-mr15">
-								<!-- user profile image -->
-                                <img src="//d1vwk06lzcci1w.cloudfront.net/50x50/{{$m->picture}}" alt="" />
-							</div>
-							<div class="hasDropdown u-floatright">
-								<button class="btn btn-sm btn-outline u-ml10">
-									ACTIONS
-									<i class="ion ion-chevron-down u-ml5"></i>
-								</button>
-							    <div class="dropdown u-mr15">
-							        this is the dropdown, without lists
-							    </div>
-							</div>
-							<div class="u-nowrap u-pt5">
-                                <span class="u-floatleft u-width30p u-nowrap"><strong>{{$m->first_name}} {{$m->last_name}}</strong> <small class="c-light">({{$m->username}})</small></span>
-                                <small class="u-floatleft u-width20p u-nowrap c-medium">{{$m->email}}</small>
-                                <small class="u-floatleft u-width10p u-nowrap c-medium">{{$m->level}}</small>
-                                <small class="u-floatleft u-width15p u-nowrap c-medium">{{$m->created_at}}</small>
-                                <small class="u-floatleft u-width25p u-nowrap c-medium">{{$m->location}}</small>
-							</div>
-						</a>
-					</li>
+                        <li>
+                            <a href="/admin/view-member/{{$m->id}}">
+                                <div class="badge badge-circle-small u-floatleft u-mt5 u-ml5 u-mr15">
+                                    <img src="//d1vwk06lzcci1w.cloudfront.net/50x50/{{$m->picture}}" alt="" />
+                                </div>
+                                <div class="hasDropdown u-floatright">
+                                    <button class="btn btn-sm btn-outline u-ml10">
+                                        ACTIONS
+                                        <i class="ion ion-chevron-down u-ml5"></i>
+                                    </button>
+                                    <div class="dropdown u-mr15">
+                                        this is the dropdown, without lists
+                                    </div>
+                                </div>
+                                <div class="u-nowrap u-pt5">
+                                    <span class="u-floatleft u-width30p u-nowrap"><strong>{{$m->first_name}} {{$m->last_name}}</strong> <small class="c-light">({{$m->username}})</small></span>
+                                    <small class="u-floatleft u-width20p u-nowrap c-medium">{{$m->email}}</small>
+                                    <small class="u-floatleft u-width10p u-nowrap c-medium">{{$m->level}}</small>
+                                    <small class="u-floatleft u-width15p u-nowrap c-medium">{{$m->created_at}}</small>
+                                    <small class="u-floatleft u-width25p u-nowrap c-medium">{{$m->location}}</small>
+                                </div>
+                            </a>
+                        </li>
                     @endforeach
 
                     @if (count($members) == 0)
-		            <li class="u-aligncenter u-pv20 bg-white">
-		                <span class="c-light">
-		                    <i class="ion ion-checkmark-circled ion-2x"></i><br />
-		                    No results
-		                </span>
-		            </li>
+                        <li class="u-aligncenter u-pv20 bg-white">
+                            <span class="c-light">
+                                <i class="ion ion-checkmark-circled ion-2x"></i><br />
+                                No results
+                            </span>
+                        </li>
                     @endif
 
                 </ul>
 
-                {!! $members->render() !!}
-			</div>
+                <div class="u-aligncenter u-mt20">
+                    {!! $members->render() !!}
+                </div>
 
-		</div>
-	</div>
+            </div>
+
+        </div>
+    </div>
 </section>
 
 @stop
