4927f2dd2bcb550ee3dbc444011a3b0857ae69c0
Guney Can Gokoglu <guneycan@gmail.com>
2016-02-21 14:02:40 +0200

    change the confirm url

--- resources/views/emails/sign_up_conf.blade.php
+++ resources/views/emails/sign_up_conf.blade.php
@@ -12,7 +12,7 @@
 
     <br /><br />
 
-    <a href="http://muhit.co/auth/confirm/{{$user->id}}/{{$user->verify_token}}" style="display: inline-block; padding: 10px 20px; line-height: 20px; background-color: #44a1e0; color: #fff; -webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; text-decoration: none;">
+    <a href="http://muhit.co/confirm/{{$user->id}}/{{$user->verify_token}}" style="display: inline-block; padding: 10px 20px; line-height: 20px; background-color: #44a1e0; color: #fff; -webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; text-decoration: none;">
         {{ trans('email.go_to_homepage') }}
     </a>
 
