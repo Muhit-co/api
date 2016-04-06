95874453261367e30af087a385b89482b5d3532d
Daniel Swakman <dswakman@oefenweb.nl>
2016-04-06 18:00:43 +0300

    updated translations

--- resources/lang/tr/error.php
+++ resources/lang/tr/error.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    "something_went_wrong" => "Oops, something went wrong...",
-    "dont_worry_explanation" => "Not to worry, we'll get you back on track in no time",
+    "something_went_wrong" => "Bir hata oldu...",
+    "dont_worry_explanation" => "Ama endişelenmeye gerek yok, siteye aşağıdaki linkten geri dönebilirsin",
     "homepage" => "Ana sayfa",
-    "report_a_bug_text" => "Or if something keeps going wrong, please let us know about it",
-    "report_a_bug_action" => "Report a bug",
+    "report_a_bug_text" => "Hatayı görmeye devam edersen lütfen bize haber ver",
+    "report_a_bug_action" => "Sorunu raporla",
 ];
