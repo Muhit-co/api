df9d57ba477666e8cc3ab64d1c50c3ca9f699c91
Daniel Swakman <dswakman@oefenweb.nl>
2016-04-06 11:17:59 +0300

    styling 404 page

---
+++ resources/lang/tr/error.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "something_went_wrong" => "Oops, something went wrong...",
+    "dont_worry_explanation" => "Not to worry, we'll get you back on track in no time",
+    "homepage" => "Ana sayfa",
+    "report_a_bug_text" => "Or if something keeps going wrong, please let us know about it",
+    "report_a_bug_action" => "Report a bug",
+];
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,41 +1,27 @@
-<html>
-<head>
-    <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+@extends('layouts.empty')
+@section('content')
 
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            height: 100%;
-            color: #B0BEC5;
-            display: table;
-            font-weight: 100;
-            font-family: 'Lato';
-        }
-
-        .container {
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
-
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
-
-        .title {
-            font-size: 72px;
-            margin-bottom: 40px;
-        }
-    </style>
-</head>
-<body>
-<div class="container">
-    <div class="content">
-        <div class="title">Page Not Found.</div>
+<div class="row" style="padding-top: 10vh;">
+  <div class="col-xs-12 u-aligncenter">
+    <img src="images/broken-link.svg" alt="" class="u-mb50" style="width: 80px; height: 80px;" />
+    <h2>{{ trans('error.something_went_wrong') }}</h2>
+    <p class="u-inlineblock u-mt20 u-mb20 c-white">{{ trans('error.dont_worry_explanation') }}:</p>
+    <div class="">
+      <a href="javascript:window.history.back()" target="_self" class="btn btn-empty u-mr10 u-mt5"><i class="ion ion-android-arrow-back ion-15x"></i></a>
+      <a href="/" class="btn btn-lg btn-primary u-mr60">
+        <i class="ion ion-android-home u-mr5"></i>
+        {{ trans('error.homepage') }}
+      </a>
     </div>
+    <div style="padding-top: 10vh;">
+      <p class="u-inlineblock c-bluedark">{{ trans('error.report_a_bug_text') }}:</p>
+      <br />
+      <a href="{{ getSupportLink() }}" class="btn btn-sm btn-quaternary u-mt10 u-opacity75" target="_blank">
+        <i class="ion ion-bug u-mr5"></i>
+        {{ trans('error.report_a_bug_action') }}
+      </a>
+    </div>
+  </div>
 </div>
-</body>
-</html>
+
+@stop
