294ebc8422974657bf32d4ef2a59bbbd281d4c1a
Daniel Swakman <dswakman@oefenweb.nl>
2016-04-20 10:20:18 +0300

    deleted old disabled announcement button

--- resources/views/announcements/index.blade.php
+++ resources/views/announcements/index.blade.php
@@ -25,17 +25,6 @@
                     <i class="ion ion-compose u-mr5"></i>
                     {{ trans('issues.post_new_announcement_cap') }}
                 </a>
-
-                <div class="u-floatright hasTooltip u-hidden">
-                    <!-- data-dialog="dialog_new_announcement" -->
-                    <a href="javascript:void(0)" class="btn btn-primary u-floatright u-ml10 btn-disabled">
-                        <i class="ion ion-compose u-mr5"></i>
-                        {{ trans('issues.post_new_announcement_cap') }}
-                    </a>
-                    <div class="tooltip u-inlineblock u-ml15" style="width: auto;">
-                        {{ trans('issues.coming_soon') }}
-                    </div>
-                </div>
             @else
                 <a href="/members/edit-profile" class="btn btn-sm btn-whiteoutline u-floatright u-ml10">
                     <i class="ion ion-edit"></i>
