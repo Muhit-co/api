aee4bc508ac4b890df9b4a4372a31c3d186274ca
boldpilot <ersoy.pembe@gmail.com>
2016-06-05 22:41:05 +0300

    muhtar view updates

--- resources/views/pages/muhtar.blade.php
+++ resources/views/pages/muhtar.blade.php
@@ -1,22 +1,6 @@
 @extends('layouts.default')
+
 @section('content')
-
-    <header class="u-relative header-list">
-
-        <div class="row u-pb40">
-            <div class="col-md-4 col-sm-7 col-md-offset-1 u-mb10">
-                <h2>{{ trans('issues.my_muhtar_info') }}</h2>
-            </div>
-            <div class="col-md-6 col-sm-5">
-                <a href="/members/edit-profile" class="btn btn-sm btn-whiteoutline u-floatright u-ml10">
-                    <i class="ion ion-edit"></i>
-                </a>
-                <?php $hood = (isset(Auth::user()->hood['name'])) ? Auth::user()->hood['name'] : '–'; ?>
-                <p class="c-white u-nowrap u-alignright"><strong>{{ $hood }}</strong> <span class="u-ml10"></span></p>
-            </div>
-        </div>
-
-    </header>
 
     <section class="u-mv20">
         <div class="row">
@@ -27,33 +11,51 @@
                     <div class="card u-mv20">
                         <div class="card-content">
 
-                            <div class="row row-nopadding">
-                                <div class="col-sm-2 u-aligncenter">
-                                    <div class="badge badge-circle-xlarge u-mr20">
-                                        <img src="//d1vwk06lzcci1w.cloudfront.net/80x80/placeholders/profile.png"
-                                             alt=""/>
+                            <div class="card u-mt0">
+                                <div class="card-header">
+                                    <div class="u-floatright">
+                                        @if(Auth::user()->id == $muhtar->id)
+                                            <a href="/user-edit" class="btn btn-outline u-mt20">
+                                                {{ trans('auth.edit_cap') }}
+                                            </a>
+                                        @endif
                                     </div>
+                                    <div class="badge badge-circle-xlarge u-floatleft u-mr20 u-mb20">
+                                        <img src="//d1vwk06lzcci1w.cloudfront.net/80x80/{{$muhtar->picture}}"
+                                             alt="{{$muhtar->full_name}}">
+                                    </div>
+                                    <h2 class="u-mt20">
+                                        {{ $muhtar->full_name }}
+                                    </h2>
+                                    <span class="">{{ $muhtar->username }}</span>
                                 </div>
-                                <div class="col-md-5 col-sm-10">
-                                    <div class="u-clearfix u-mb20">
-                                        <h2 class="u-mt20">{!! $muhtar->full_name !!}</h2>
-                                        <p>Muhtar</p>
-                                    </div>
-                                    <i class="ion ion-android-call ion-fw ion-2x u-aligncenter u-floatleft c-blue u-mt5 u-mr10"></i>
-                                    <label>{{ trans('auth.phone_number') }}</label>
-                                    <p class="u-mb20">{{ $muhtar->phone }}</p>
+                                <div class="card-content">
 
-                                    <i class="ion ion-email ion-fw ion-2x u-aligncenter u-floatleft c-blue u-mt5 u-mr10"></i>
-                                    <label>{{ trans('auth.email_address') }}</label>
-                                    <p class="u-mb20">{{ $muhtar->email }}</p>
-                                </div>
-                                <div class="col-md-5 u-pt20">
-                                    <label class="c-blue">Muhtarlık</label>
-                                    <p>{{ $muhtar->location }}</p>
-                                    <div class="media u-mt20">
-                                        {{--<div class="media-map">--}}
-                                            {{--<img src="images/map.jpg" alt=""/>--}}
-                                        {{--</div>--}}
+                                    <div class="row u-mv10">
+                                        <div class="col-xs-2 u-aligncenter u-pr30">
+                                            <i class="ion ion-location ion-2x c-light"></i>
+                                        </div>
+                                        <div class="col-xs-9">
+                                            <div class="u-floatleft u-mv5">{!! $muhtar->location !!}</div>
+                                        </div>
+                                    </div>
+                                    <div class="row u-mv10">
+                                        <div class="col-xs-2 u-aligncenter u-pr30">
+                                            <i class="ion ion-email ion-2x c-light"></i>
+                                        </div>
+                                        <div class="col-xs-9">
+                                            <div class="u-floatleft u-mv5">{{ $muhtar->email }}</div>
+                                        </div>
+                                    </div>
+                                    <div class="row u-mv10">
+                                        <div class="col-xs-2 u-aligncenter u-pr30">
+                                            <i class="ion ion-calendar ion-15x c-light u-mt5"></i>
+                                        </div>
+                                        <div class="col-xs-9">
+                                            <div class="u-floatleft u-mv5">
+                                                {!! Date::parse($muhtar->created_at)->format('d F Y') !!}
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
