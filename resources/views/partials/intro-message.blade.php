44d88557490a8f598fbeeaa9a65526b6a406f1e9
L Daniel Swakman <d.swakman@gmail.com>
2016-08-01 22:46:34 +0300

    updated intro message text

--- resources/views/partials/intro-message.blade.php
+++ resources/views/partials/intro-message.blade.php
@@ -10,11 +10,9 @@
                 <div class="col-xs-10 u-aligncenter">
                     <i class="ion ion-muhit-tea-white u-floatleft u-mr10" style="margin-top: 3px;"></i>
                     <div class="col-xs-hide">
-                        <span class="message-expanded">
-                            <h3 class="u-floatleft u-mr20">Muhit'e hoş geldin!</h3>
-                        </span>
+                        <h3 class="u-floatleft u-mr20">Muhit'e hoş geldin!</h3>
                     </div>
-                    <a href="javascript:void(0)" id="message_expand" class="btn btn-sm btn-empty" style="margin-top: -2px;">
+                    <a href="javascript:void(0)" id="message_expand" class="btn btn-sm btn-empty u-mr50" style="margin-top: -2px;">
                         {{ trans('intro.how_does_it_work_cap') }}
                         <span class="message-expanded">
                             <i class="ion ion-chevron-down u-ml5"></i>
@@ -28,30 +26,25 @@
 
             <div class="message-expanded u-hidden">
 
-                <div class="u-ph15 u-mt20">
+                <div class="u-ph15 u-mt20 u-aligncenter">
                     <h2>{{ trans('intro.title') }}</h2>
                 </div>
 
                 <div class="row u-aligncenter u-pv20">
-                    <div class="col-sm-3 col-xs-6 u-pv20">
+                    <div class="col-sm-4 col-xs-12 u-pv20">
                         <i class="ion ion-iphone ion-3x"></i>
-                        <h3 class="u-mb25">{{ trans('intro.usp1_title') }}<br /><br /></h3>
+                        <h3>{{ trans('intro.usp1_title') }}<br /><br /></h3>
                         <p class="c-white u-alignleft"><small>{{ trans('intro.usp1_text') }}</small></p>
                     </div>
-                    <div class="col-sm-3 col-xs-6 u-pv20">
+                    <div class="col-sm-4 col-xs-12 u-pv20">
                         <i class="ion ion-person-stalker ion-3x"></i>
                         <h3>{{ trans('intro.usp2_title') }}<br /><br /></h3>
                         <p class="c-white u-alignleft"><small>{{ trans('intro.usp2_text') }}</small></p>
                     </div>
-                    <div class="col-sm-3 col-xs-6 u-pv20">
+                    <div class="col-sm-4 col-xs-12 u-pv20">
                         <i class="ion ion-speakerphone ion-3x"></i>
                         <h3>{{ trans('intro.usp3_title') }}<br /><br /></h3>
                         <p class="c-white u-alignleft"><small>{{ trans('intro.usp3_text') }}</small></p>
-                    </div>
-                    <div class="col-sm-3 col-xs-6 u-pv20">
-                        <i class="ion ion-paper-airplane ion-3x"></i>
-                        <h3>{{ trans('intro.usp4_title') }}<br /><br /></h3>
-                        <p class="c-white u-alignleft"><small>{{ trans('intro.usp4_text') }}</small></p>
                     </div>
                 </div>
                 <div class="row u-aligncenter u-pv10">
