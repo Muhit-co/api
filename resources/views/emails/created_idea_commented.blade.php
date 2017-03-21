da9983080568beb52533877d8b6c6955f52801f1
L Daniel Swakman <d.swakman@gmail.com>
2017-03-21 15:15:12 +0100

    email job trying

--- resources/views/emails/created_idea_commented.blade.php
+++ resources/views/emails/created_idea_commented.blade.php
@@ -2,7 +2,7 @@
 
 @section('title')
 
-    {{ trans('email.created_idea_commented_title', array('sender' => $comment->muhtar->first_name)) }}
+    {{ trans('email.created_idea_commented_title', array('sender' => $comment_user->first_name)) }}
 
 @stop
 
@@ -10,7 +10,7 @@
 
     @include('emails.partials.header', array('username' => $user->first_name ))
 
-    {{ trans('email.created_idea_commented_content', array('sender' => $comment->muhtar->first_name)) }}
+    {{ trans('email.created_idea_commented_content', array('sender' => $comment_user->first_name)) }}
 
     <br /><br />
 
