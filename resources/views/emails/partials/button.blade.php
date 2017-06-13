7df296a560a32ee5749d9b057328e0f03c56321c
L Daniel Swakman <d.swakman@gmail.com>
2017-06-13 21:56:05 +0200

    typo fix

--- resources/views/emails/partials/button.blade.php
+++ resources/views/emails/partials/button.blade.php
@@ -1,4 +1,4 @@
-<?phps
+<?php
 $url .= '?utm_source=transactional&utm_medium=email&utm_campaign=action_button';
 ?>
 <a href="{{ $url }}" style="display: inline-block; padding: 10px 20px; line-height: 20px; background-color: #44a1e0; color: #fff; -webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; text-decoration: none;">
