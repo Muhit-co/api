b5e631db2b8e83e9759071d7cac989c6e983c779
L Daniel Swakman <d.swakman@gmail.com>
2017-07-01 16:44:31 +0300

    improved member index view

--- resources/views/admin/members/index.blade.php
+++ resources/views/admin/members/index.blade.php
@@ -15,9 +15,9 @@
                     <form method="get">
 
                         <div class="form-group u-floatleft u-width150">
-                            <input class="form-input" name="q" type="text" placeholder="Search..." value="<?php echo $param_q ?>" />
+                            <input class="form-input" name="q" type="text" placeholder="Search..." value="{{ $param_q }}" />
                             @if (strlen($param_q) > 0)
-                                <a href="<?php echo '?' . buildRelativeUrl('q', '') ?>" class="form-appendRight u-ph15" style="padding-top: 8px;"><i class="ion ion-close"></i></a>
+                                <a href="{{ '?' . buildRelativeUrl('q', '') }}" class="form-appendRight u-ph15" style="padding-top: 8px;"><i class="ion ion-close"></i></a>
                             @endif
                         </div>
 
@@ -35,18 +35,16 @@
 
     <div class="row">
         <div class="col-xs-12">
-            <?php
-            $active_tab = (isset($_GET['level'])) ? intval($_GET['level']) : null;
-            ?>
+            <?php $active_tab = (isset($_GET['level'])) ? intval($_GET['level']) : null; ?>
             <ul class="tabs">
                 <li>
-                    <a href="{{ '/' . Request::path() }}" <?php echo ($active_tab === null) ? 'class="active"' : ''; ?> >
+                    <a href="{{ '/' . Request::path() }}" {{ $active_tab === null ? 'class="active"' : '' }} >
                         ALL
                     </a>
                 </li>
                 @foreach ([0, 3, 4, 5, 10] as $t)
                     <li>
-                        <a href="<?php echo '?' . buildRelativeUrl('level', $t) ?>" <?php echo ($active_tab === $t) ? 'class="active"' : ''; ?> >
+                        <a href="{{ '?' . buildRelativeUrl('level', $t) }}" {{ $active_tab === $t ? 'class="active"' : '' }}>
                             {{ strtoupper( getUserLevel($t, true) ) }}
                         </a>
                     </li>
@@ -70,13 +68,13 @@
             <div class="list list-small list-overflowshow">
                 <div class="list-header u-pr110 u-relative">
                     <div class="row row-nopadding">
-                        <div class="col-xs-10 col-sm-4">
+                        <div class="col-xs-10 col-sm-3">
                             <small class="u-nowrap u-ml40"><em>name (username)</em></small>
                         </div>
                         <div class="col-sm-3 col-sm-hide">
                             <small class="u-nowrap"><em>email</em></small>
                         </div>
-                        <div class="col-sm-1 col-sm-hide">
+                        <div class="col-sm-3 col-sm-hide">
                             <small class="u-nowrap"><em>level</em></small>
                         </div>
                         <div class="col-sm-2 col-sm-hide">
@@ -85,9 +83,7 @@
                     </div>
                     <div class="u-pinned-topright u-ma10">
                         <small class="c-medium">
-                            {{count($members)}} record<?php if (count($members) != 1) {
-                                echo 's';
-} ?>
+                            {{count($members)}} record{{ count($members) != 1 ? 's' : '' }}
                         </small>
                     </div>
                 </div>
@@ -97,12 +93,12 @@
                         <li class="u-relative">
                             <a href="/admin/view-member/{{$m->id}}" class="u-pr110">
                                 <div class="row row-nopadding">
-                                    <div class="col-sm-4">
+                                    <div class="col-sm-3">
                                         <div class="badge badge-circle-small u-floatleft u-mt5 u-ml5 u-mr15">
                                             <img src="{{ getImageURL($m->picture, '50x50') }}" alt="" class="u-block" />
                                         </div>
-                                        <div class="u-nowrap u-pt5">
-                                            <span class="u-floatleft u-nowrap"><strong>{{$m->first_name}} {{$m->last_name}}</strong> <small class="c-light">({{$m->username}})</small></span>
+                                        <div class="u-nowrap u-pt5 c-bluedark">
+                                            <small class="u-nowrap">{{$m->first_name}} {{$m->last_name}}</small>
                                         </div>
                                     </div>
                                     <div class="col-sm-3 col-sm-hide">
@@ -110,27 +106,22 @@
                                             <small class="u-nowrap">{{$m->email}}</small>
                                         </div>
                                     </div>
-                                    <div class="col-sm-1 col-sm-hide">
+                                    <div class="col-sm-3 col-sm-hide">
                                         <div class="u-nowrap u-pt5 c-medium" title="level {{ $m->level }}">
-                                            <small class="u-nowrap">{{ getUserLevel($m->level) }}</small>
+                                            <small class="u-nowrap">
+                                                {{ getUserLevel($m->level) }}
+                                                {{ isset($m->admin_type) ? '(' . $m->admin_type . ')' : ''}}
+                                            </small>
                                         </div>
                                     </div>
-                                    <div class="col-sm-4 col-sm-hide">
+                                    <div class="col-sm-3 col-sm-hide">
                                         <div class="u-nowrap u-pt5 c-medium">
                                             <small class="u-nowrap">{{$m->location}}</small>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-4 col-sm-hide">
-                                        <div class="u-nowrap u-pt5 c-medium">
-                                            <small class="u-nowrap">{{$m->admin_type}}</small>
                                         </div>
                                     </div>
                                 </div>
                             </a>
                             <div class="u-pinned-topright u-ma10">
-                                <a href="/admin/view-member/{{$m->id}}" class="btn btn-sm btn-outline u-floatleft col-sm-hide">
-                                    VIEW
-                                </a>
                                 <div class="hasDropdown u-floatleft">
                                     <button class="btn btn-sm btn-outline u-ml10">
                                         ACTIONS
