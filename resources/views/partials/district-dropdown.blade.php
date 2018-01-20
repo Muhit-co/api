e422614e1ded84bfca96148f52f3684fd5ddf62f
Anıl Uygun <aniluyg@gmail.com>
2018-01-20 12:49:15 +0300

    fix district sorting issue in homepage

--- resources/views/partials/district-dropdown.blade.php
+++ resources/views/partials/district-dropdown.blade.php
@@ -10,20 +10,16 @@
 
     <div class="row row-nopadding">
         @foreach($all_districts as $d)
-            @if($d->issues->count() > 0)
-                <div class="col-md-3 col-sm-4 col-xs-6">
-                    <a href="/fikirler?{{ buildRelativeUrl('district', $d->name . ', ' . $d->city->name, 'location' ) }}" class="u-block u-lineheight20 u-pb10 u-truncate u-mr5" title="{{ $d->name . ', ' . $d->city->name }}">
-                        <?php if(isset($district) && $d->name === $district->name) { echo '<strong>'; } ?>
-                        <span class="u-mr5">{{ $d->name }}</span>
-                        <div class="u-sm-show"></div>
-                        <small class="u-opacity75">{{ $d->city->name }}</small>
-                        @if($d->issues->count() > 0)
-                            <span class="c-light">({{ $d->issues->count() }})</span>
-                        @endif
-                        <?php if(isset($district) && $d->name === $district->name) { echo '</strong>'; } ?>
-                    </a>
-                </div>
-            @endif
+            <div class="col-md-3 col-sm-4 col-xs-6">
+                <a href="/fikirler?{{ buildRelativeUrl('district', $d->name . ', ' . $d->city_name, 'location' ) }}" class="u-block u-lineheight20 u-pb10 u-truncate u-mr5" title="{{ $d->name . ', ' . $d->city_name }}">
+                    <?php if(isset($district) && $d->name === $district->name) { echo '<strong>'; } ?>
+                    <span class="u-mr5">{{ $d->name }}</span>
+                    <div class="u-sm-show"></div>
+                    <small class="u-opacity75">{{ $d->city_name }}</small>
+                    <span class="c-light">({{ $d->issue_count }})</span>
+                    <?php if(isset($district) && $d->name === $district->name) { echo '</strong>'; } ?>
+                </a>
+            </div>
         @endforeach
     </div>
 
