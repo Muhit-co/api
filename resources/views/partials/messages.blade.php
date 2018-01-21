4ba8bf26a6411d2e5b4b97c3e5dfd5687357f601
L Daniel Swakman <d.swakman@gmail.com>
2018-01-21 20:09:30 +0300

    fixes in partials

--- resources/views/partials/messages.blade.php
+++ resources/views/partials/messages.blade.php
@@ -1,6 +1,6 @@
 @if (session('success'))
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-xs-12">
             <div class="flash flash-success u-mv10">
                 <a href="javascript:void(0)" id="flash_close">
                     <i class="ion ion-android-close ion-15x u-floatright u-ml10 u-mb10"></i>
@@ -16,26 +16,26 @@
 @endif
 @if (session('warning'))
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-xs-12">
             <div class="flash flash-warning u-mv10">
                 <a href="javascript:void(0)" id="flash_close">
                     <i class="ion ion-android-close ion-15x u-floatright u-ml10 u-mb10"></i>
                 </a>
                 <i class="ion ion-information-circled ion-15x u-floatleft u-mr10 u-mb10"></i>
-                {{ session('warning')  }}
+                {{ session('warning') }}
             </div>
         </div>
     </div>
 @endif
 @if (session('error'))
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-xs-12">
             <div class="flash flash-error u-mv10">
                 <a href="javascript:void(0)" id="flash_close">
                     <i class="ion ion-android-close ion-15x u-floatright u-ml10 u-mb10"></i>
                 </a>
                 <i class="ion ion-alert-circled ion-15x u-floatleft u-mr10 u-mb10"></i>
-                {{ session('error')  }}
+                {{ session('error') }}
             </div>
         </div>
     </div>
