de7528ba1db78f66bb943ba6ce3bbf0de2a09ca9
L Daniel Swakman <d.swakman@gmail.com>
2018-01-21 20:34:23 +0300

    issue update list improvements

--- resources/views/partials/issues-latest-updates.blade.php
+++ resources/views/partials/issues-latest-updates.blade.php
@@ -11,19 +11,22 @@
 
 			<li>
 				<a href="/issues/view/{{$issue->id}}">
-					<div class="badge badge-circle-small badge-support-outline badge-{{ $issue_status['class'] }} u-floatleft u-mr10 u-mt5">
+					<div class="badge badge-circle badge-support-outline badge-{{ $issue_status['class'] }} u-floatleft u-mr10 u-mt5">
 						<i class="ion {{ $issue_status['icon'] }}"></i>
 					</div>
 					<div class="u-ml55 u-lineheight20">
 						<small>
-							<div class="u-opacity75 u-mb5 c-medium">
-								<div class="u-floatright u-opacity75">
-                                    <?php echo strftime('%d %h %Y %H:%M:%S', strtotime($issue->updated_at)) ?>
+							<div class="u-mb5">
+								<div class="u-floatright u-opacity75 hasTooltip">
+									X gün önce
+                                    <div class="tooltip tooltip-compact tooltip-alignright" style="width: auto; white-space: nowrap;">
+                                    	{{ strftime('%d %h %Y %H:%M:%S', strtotime($issue->updated_at)) }}
+                                    </div>
 								</div>
 								FİKİR GÜNCELLENDİ
 							</div>
 							<h4>{{ $issue->title }}</h4>
-							<strong class="c-light"><i class="ion ion-chatbox u-mr5"></i> {{ $issue->commenter }}</strong>
+							<strong class="c-medium u-opacity75"><i class="ion ion-chatbox u-mr5"></i> {{ $issue->commenter }}</strong>
 						</small>
 					</div>
 				</a>
